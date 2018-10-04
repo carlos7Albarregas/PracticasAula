65f673241b856cd13dd936edf4254d958fb0ba68
Carlos-PC\Carlos <carlos4cinf@gmail.com>
2018-10-04 13:32:11 +0200

    resolviendo servletRegistro (quedan preferencias)

--- src/main/java/es/albarregas/servlets/ServletRegistro.java
+++ src/main/java/es/albarregas/servlets/ServletRegistro.java
@@ -143,7 +143,7 @@ public class ServletRegistro extends HttpServlet {
                     out.println("<option value='29'>29</option>");
                     out.println("<option value='30'>30</option>");
                     out.println("<option value='31'>31</option>");
-              
+                    
                 out.println("/</select>");
                 out.println("/");
                 out.println("<select name='mes'>");
@@ -214,13 +214,100 @@ public class ServletRegistro extends HttpServlet {
                     /*Simplemente aqui lo que hago es recoger lo que haya seleccionado el usuario. Para ello indicandolo
                     en la propiedad value y selected para que lo seleccione lo tendriamos ya*/
                     out.println("<select name='dia' id='dia'>");
-                    out.println("<option value="+dia+" selected>"+dia+"</option>");
+                    out.println("<option disabled value="+dia+" selected>"+dia+"</option>");
+                    out.println("<option value='1'>1</option>");
+                    out.println("<option value='2'>2</option>");
+                    out.println("<option value='3'>3</option>");
+                    out.println("<option value='4'>4</option>");
+                    out.println("<option value='5'>5</option>");
+                    out.println("<option value='6'>6</option>");
+                    out.println("<option value='7'>7</option>");
+                    out.println("<option value='8'>8</option>");
+                    out.println("<option value='9'>9</option>");
+                    out.println("<option value='10'>10</option>");
+                    out.println("<option value='11'>11</option>");
+                    out.println("<option value='12'>12</option>");
+                    out.println("<option value='13'>13</option>");
+                    out.println("<option value='14'>14</option>");
+                    out.println("<option value='15'>15</option>");
+                    out.println("<option value='16'>16</option>");
+                    out.println("<option value='17'>17</option>");
+                    out.println("<option value='18'>18</option>");
+                    out.println("<option value='19'>19</option>");
+                    out.println("<option value='20'>20</option>");
+                    out.println("<option value='21'>21</option>");
+                    out.println("<option value='22'>22</option>");
+                    out.println("<option value='23'>23</option>");
+                    out.println("<option value='24'>24</option>");
+                    out.println("<option value='25'>25</option>");
+                    out.println("<option value='26'>26</option>");
+                    out.println("<option value='27'>27</option>");
+                    out.println("<option value='28'>28</option>");
+                    out.println("<option value='29'>29</option>");
+                    out.println("<option value='30'>30</option>");
+                    out.println("<option value='31'>31</option>");
+                    
                     out.println("</select>");
+                    
                     out.println("<select name='mes' id='mes'>");
-                    out.println("<option value="+mes+" selected>"+mes+"</option>");
+                    out.println("<option disabled value="+mes+" selected>"+mes+"</option>");
+                    out.println("<option value='1'>Enero</option>");
+                    out.println("<option value='2'>Febrero</option>");
+                    out.println("<option value='3'>Marzo</option>");
+                    out.println("<option value='4'>Abril</option>");
+                    out.println("<option value='5'>Mayo</option>");
+                    out.println("<option value='6'>Junio</option>");
+                    out.println("<option value='7'>Julio</option>");
+                    out.println("<option value='8'>Agosto</option>");
+                    out.println("<option value='9'>Septiembre</option>");
+                    out.println("<option value='10'>Octubre</option>");
+                    out.println("<option value='11'>Noviembre</option>");
+                    out.println("<option value='12'>Diciembre</option>");
                     out.println("</select>");
+                    
                     out.println("<select name='anyo' id='anyo'>");
-                    out.println("<option value="+anio+" selected>"+anio+"</option>");
+                    out.println("<option disabled value="+anio+" selected>"+anio+"</option>");
+                    out.println("<option value='1970'>1970</option>");
+                        out.println("<option value='1971'>1971</option>");
+                        out.println("<option value='1972'>1972</option>");
+                        out.println("<option value='1973'>1973</option>");
+                        out.println("<option value='1974'>1974</option>");
+                        out.println("<option value='1975'>1975</option>");
+                        out.println("<option value='1976'>1976</option>");
+                        out.println("<option value='1977'>1977</option>");
+                        out.println("<option value='1978'>1978</option>");
+                        out.println("<option value='1979'>1979</option>");
+                        out.println("<option value='1980'>1980</option>");
+                        out.println("<option value='1981'>1981</option>");
+                        out.println("<option value='1982'>1982</option>");
+                        out.println("<option value='1983'>1983</option>");
+                        out.println("<option value='1984'>1984</option>");
+                        out.println("<option value='1985'>1985</option>");
+                        out.println("<option value='1986'>1986</option>");
+                        out.println("<option value='1987'>1987</option>");
+                        out.println("<option value='1988'>1988</option>");
+                        out.println("<option value='1989'>1989</option>");
+                        out.println("<option value='1990'>1990</option>");
+                        out.println(" <option value='1991'>1991</option>");
+                        out.println("<option value='1992'>1992</option>");
+                        out.println("<option value='1993'>1993</option>");
+                        out.println("<option value='1994'>1994</option>");
+                        out.println("<option value='1995'>1995</option>");
+                        out.println("<option value='1996'>1996</option>");
+                        out.println("<option value='1997'>1997</option>");
+                        out.println("<option value='1998'>1998</option>");
+                        out.println("<option value='1999'>1990</option>");
+                        out.println("<option value='2000'>2000</option>");
+                        out.println("<option value='2001'>2001</option>");
+                        out.println("<option value='2002'>2002</option>");
+                        out.println("<option value='2003'>2003</option>");
+                        out.println("<option value='2004'>2004</option>");
+                        out.println("<option value='2005'>2005</option>");
+                        out.println("<option value='2006'>2006</option>");
+                        out.println("<option value='2007'>2007</option>");
+                        out.println("<option value='2008'>2008</option>");
+                        out.println("<option value='2009'>2009</option>");
+                        out.println("<option value='2010'>2010</option>");
                     out.println("</select>");
                     
                     out.println("<img src='imagenes/check.jpg' heigh=20px width=20px");
@@ -266,16 +353,18 @@ public class ServletRegistro extends HttpServlet {
             /* TERCER FIELDSET*/
             out.println("<fieldset id='tercero'>");
                 
-                out.println("<legend>Informacion general</legend>");
-                out.println("<label for='pref'>Preferencias</label><br/>");
-                out.println("<input type='checkbox' name='pref' value='deporte' />Deporte");
-                out.println("<br/>");
-                out.println("<input type='checkbox' name='pref' value='lectura' />Lectura");
-                out.println("<br/>");
-                out.println("<input type='checkbox' name='pref' value='cine' />Cine");
-                out.println("<br/>");
-                out.println("<input type='checkbox' name='pref' value='viajes' />Viajes");
-                
+            out.println("<legend>Informacion general</legend>");
+            out.println("<label for='pref'>Preferencias</label><br/>");
+               
+            out.println("<input type='checkbox' name='pref' value='deporte' />Deporte");
+            out.println("<br/>");
+            out.println("<input type='checkbox' name='pref' value='lectura' />Lectura");
+            out.println("<br/>");
+            out.println("<input type='checkbox' name='pref' value='cine' />Cine");    
+            out.println("<br/>");
+            out.println("<input type='checkbox' name='pref' value='viajes' />Viajes");   
+            out.println("<br/>");
+ 
             out.println("</fieldset>");
             
             out.println("<input type='submit' name='Submit' id='button' value='Enviar'/>");
